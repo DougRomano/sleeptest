d8f0eb6e91b86be7203710884579384faeda2eef
Doug Romano <romano@csgsolutions.com>
2021-04-16 12:01:24 -0500

    first commit

---
+++ Controllers/SleepController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace sleepTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SleepController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<SleepController> _logger;
+
+        public SleepController(ILogger<SleepController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("info")]
+        public string Info()
+        {
+            
+            return  "ok fine";
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
